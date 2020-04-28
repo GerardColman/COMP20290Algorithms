c5dc67d1c02b89da7d505357cc1158366395b957
Gerard Colman <gerard.colman1@gmail.com>
2020-04-28 16:41:31 +0100

    Added timings

--- src/HuffmanAssignment/HuffmanAlgorithm.java
+++
@@ -1,5 +0,0 @@
-package HuffmanAssignment;
-
-public class HuffmanAlgorithm {
-
-}
--- src/Practical1/RussianPeasant.java
+++ src/Practical1/RussianPeasant.java
@@ -1,20 +1,29 @@
 package Practical1;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.lang.System.*;
 
 public class RussianPeasant {
 
-    public static void main(String[] args){
-        int x = 68;
-        int y = 139;
-        RussianPeasant RP = new RussianPeasant();
-        final long startTime = System.nanoTime();
-        RP.RussianMultiply(x, y);
-        final long elapsedTime = System.nanoTime() - startTime;
-        System.out.println(x + " * " + y + " = " + RP.RussianMultiply(x,y));
-        System.out.println("Time Taken: " + elapsedTime);
-
+    public static void main(String[] args) throws IOException {
+        int x = 0;
+        int y = 0;
+        BufferedWriter writer = new BufferedWriter(new FileWriter("timings.txt"));
+        writer.write("X * Y | elapsed time" + "\n");
+        for(x = 0;x<1000;x++){
+            RussianPeasant RP = new RussianPeasant();
+            final long startTime = System.nanoTime();
+            RP.RussianMultiply(x, y);
+            final long elapsedTime = System.nanoTime() - startTime;
+            System.out.print(x + " * " + y + " = " + RP.RussianMultiply(x,y));
+            System.out.println(" | " + elapsedTime);
+            writer.write(x + "*" + y + " | " + elapsedTime + "\n");
+            y++;
+        }
+        writer.close();
     }
 
     public int RussianMultiply(int x, int y){
